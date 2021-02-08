3390db193f435bc52aef79322976019fc7ffd1cf
aMaro_ <nowosiadlymarek@gmail.com>
2021-02-08 21:43:39 +0100

    feat: adjust proxy server to preparing request and forwarding response

--- proxyserver/config/config.go
+++ proxyserver/config/config.go
@@ -5,23 +5,23 @@ import (
 	"log"
 )
 
-type serverConfig struct {
+type ServerConfig struct {
 	Port           string `env:"PORT" envDefault:"4000" validate:"numeric"`
 	GinMode        string `env:"GIN_MODE envDefault:"debug" validate:"oneof="debug release"`
 	FinalServerUrl string `env:"FINAL_SERVER_URL" envDefault:"http://localhost:3000/experiment" validate:"url"`
 }
 
-var serverConfigInstance *serverConfig
+var serverConfigInstance *ServerConfig
 
-func GetServerConfig() *serverConfig {
+func GetServerConfig() *ServerConfig {
 	if serverConfigInstance == nil {
-		err, conf := envloader.Load(serverConfig{})
+		err, conf := envloader.Load(ServerConfig{})
 
 		if err != nil {
 			log.Fatal("Server config loading failed")
 		}
 
-		serverConfigInstance = conf.(*serverConfig)
+		serverConfigInstance = conf.(*ServerConfig)
 	}
 
 	return serverConfigInstance
--- proxyserver/main.go
+++ proxyserver/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/amaro0/microservices-fault-tolerance-experiments/proxyserver/config"
 	"github.com/gin-gonic/gin"
-	"io/ioutil"
+	"github.com/google/uuid"
 	"log"
 	"net/http"
+	"net/url"
 	"time"
 )
+
+type Result struct {
+	Error string            `json:"error"`
+	Data  FinalServerResult `json:"data"`
+}
+
+type FinalServerResult struct {
+	Hashed string `json:"hashed"`
+}
 
 func main() {
 	serverConfig := config.GetServerConfig()
@@ -22,26 +33,52 @@ func main() {
 
 	r.GET("/proxy", func(c *gin.Context) {
 		time.Sleep(300 * time.Millisecond)
-
-		resp, err := http.Get(serverConfig.FinalServerUrl)
-
+		url, err := createExperimentUrl(serverConfig)
 		if err != nil {
-			log.Println("Request error! ", err.Error())
+			c.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
 		}
 
+		resp, err := http.Get(url.String())
 		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
 
 		if err != nil {
 			c.JSON(500, gin.H{
-				"error": err,
+				"error": err.Error(),
 			})
+			return
+		}
+
+		result := Result{}
+		json.NewDecoder(resp.Body).Decode(&result)
+
+		if resp.StatusCode > 299 {
+			log.Println(result.Error)
+			c.JSON(500, gin.H{
+				"error": "Experiment returned " + resp.Status,
+			})
+			return
 		}
 
 		c.JSON(200, gin.H{
-			"data": body,
+			"data": result.Data,
 		})
 	})
 
 	r.Run(":" + serverConfig.Port)
+}
+
+func createExperimentUrl(serverConfig *config.ServerConfig) (url.URL, error) {
+	base, err := url.Parse(serverConfig.FinalServerUrl)
+	if err != nil {
+		return *base, err
+	}
+
+	query := url.Values{}
+	query.Add("stringToHash", uuid.NewString())
+	base.RawQuery = query.Encode()
+
+	return *base, nil
 }
