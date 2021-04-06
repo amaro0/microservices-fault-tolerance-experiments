56cb77a9aa236f40f0521240d18e7a276f614f93
Marek Nowosiadły <nowosiadlymarek@gmail.com>
2021-04-06 22:20:47 +0200

    feat: add file mode to final server

--- finalserver/config/config.go
+++ finalserver/config/config.go
@@ -6,8 +6,9 @@ import (
 )
 
 type serverConfig struct {
-	Port    string `env:"PORT" envDefault:"3000" validate:"numeric"`
-	GinMode string `env:"GIN_MODE envDefault:"debug" validate:"oneof="debug release"`
+	Port     string `env:"PORT" envDefault:"3000" validate:"numeric"`
+	GinMode  string `env:"GIN_MODE" envDefault:"debug" validate:"oneof=debug release"`
+	FailMode string `env:"FILE_MODE" envDefault:"none" validate:"oneof=none timeout"`
 }
 
 var serverConfigInstance *serverConfig
