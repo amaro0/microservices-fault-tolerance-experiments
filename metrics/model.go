24aec562d5cadb454708fea4aabe369147128d7c
Marek Nowosiadły <nowosiadlymarek@gmail.com>
2021-04-20 23:44:16 +0200

    feat: add csv parsing to metrics model

--- metrics/model.go
+++ metrics/model.go
@@ -12,7 +12,23 @@ type Model struct {
 	Server      ServerType `json:"server" binding:"required"`
 	RequestId   string     `json:"requestId" binding:"required"`
 	WasError    bool       `json:"wasError"`
+	ErrorType   string     `json:"errorType"`
 	ErrorTime   int        `json:"errorTime"`
 	SuccessTime int        `json:"successTime"`
-	ErrorType   string     `json:"errorType"`
+}
+
+func (m *Model) prepareForCSV() []string {
+	var wasErrorInt int8
+	if m.WasError {
+		wasErrorInt = 1
+	}
+
+	return []string{
+		string(m.Server),
+		m.RequestId,
+		string(wasErrorInt),
+		m.ErrorType,
+		string(m.ErrorTime),
+		string(m.SuccessTime),
+	}
 }
