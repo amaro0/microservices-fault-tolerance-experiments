32112bf82e1f3e941788dd1a1c2723f870788450
Marek Nowosiadły <nowosiadlymarek@gmail.com>
2021-04-28 19:16:52 +0200

    feat: add proxy protection type to env

--- proxyserver/config/config.go
+++ proxyserver/config/config.go
@@ -5,12 +5,20 @@ import (
 	"log"
 )
 
+type ProxyProtectionType string
+
+const (
+	Timeout        ProxyProtectionType = "timeout"
+	CircuitBreaker ProxyProtectionType = "breaker"
+)
+
 type ServerConfig struct {
-	Port                string `env:"PORT" envDefault:"4000" validate:"numeric"`
-	GinMode             string `env:"GIN_MODE envDefault:"debug" validate:"oneof="debug release"`
-	FinalServerUrl      string `env:"FINAL_SERVER_URL" envDefault:"http://localhost:3000/experiment" validate:"url"`
-	FinalInstancesCount int    `env:"FINAL_INSTANCES_COUNT" envDefault:"1" validate:"number,min=1"`
-	MetricsServerUrl    string `env:"METRICS_SERVER_URL" envDefault:"http://localhost:2000" validate:"url"`
+	Port                 string                `env:"PORT" envDefault:"4000" validate:"numeric"`
+	GinMode              string                `env:"GIN_MODE envDefault:"debug" validate:"oneof="debug release"`
+	FinalServerUrl       string                `env:"FINAL_SERVER_URL" envDefault:"http://localhost:3000/experiment" validate:"url"`
+	FinalInstancesCount  int                   `env:"FINAL_INSTANCES_COUNT" envDefault:"1" validate:"number,min=1"`
+	MetricsServerUrl     string                `env:"METRICS_SERVER_URL" envDefault:"http://localhost:2000" validate:"url"`
+	ProxyProtectionTypes []ProxyProtectionType `env:"PROXY_PROTECTION_TYPE" envDefault:""`
 }
 
 var serverConfigInstance *ServerConfig
